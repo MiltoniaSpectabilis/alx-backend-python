b4e47e68e5b8ee97ddd62a2cf54b2d60c663a3d0
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-10-11 00:44:54 +0100

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Defines a type-annotated function floor"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of a float"""
+    return math.floor(n)
