73c126ce97d8694f367781572a4e6ed9f124c07a
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-10-11 06:36:53 +0100

    another attempt

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,28 +1,20 @@
 #!/usr/bin/env python3
 
-"""Defines a type-annotated function zoom_array."""
+"""Defines a type-annotated function"""
+
+from typing import List
 
 
-from typing import Tuple, List, TypeVar, Union
-
-T = TypeVar('T')
-
-
-def zoom_array(lst: Tuple[T, ...], factor: int = 2) -> List[T]:
-    """
-    Repeats each element in the tuple a specified number of times.
-    """
-    zoomed_in: List[T] = [
+def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
+    zoomed_in: List[int] = [
         item for item in lst
-        for _ in range(factor)
+        for i in range(factor)
     ]
     return zoomed_in
 
 
 array = [12, 72, 91]
 
-zoom_2x = zoom_array(tuple(array))
-zoom_3x = zoom_array(tuple(array), 3)
+zoom_2x = zoom_array(array)
 
-print(zoom_2x)
-print(zoom_3x)
+zoom_3x = zoom_array(array, 3)
