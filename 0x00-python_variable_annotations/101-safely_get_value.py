7403d06394757b3b9a04c9f1aa0a07b26c8f92d6
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-10-11 03:35:13 +0100

    fix syntax

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
+"""Defines a type-annotated function safely_get_value."""
+
 from typing import Mapping, Any, TypeVar, Union, Optional
 
 T = TypeVar('T')
 
-"""Defines a type-annotated function safely_get_value."""
 
-
-def safely_get_value(dct: Mapping[Any, Any], key: Any,
-                     default: Optional[T] = None) -> Union[Any, T]:
+def safely_get_value(
+    dct: Mapping[Any, Any], key: Any, default: Optional[T] = None
+) -> Union[Any, T]:
     """
     Safely gets the value from a dictionary for a given key,
     or returns a default value.
