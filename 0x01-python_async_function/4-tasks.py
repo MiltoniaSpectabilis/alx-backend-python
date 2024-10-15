bdae24bc4ba65ba15d020858f67f0846b81e7dd8
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-10-15 02:19:16 +0100

    Tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Module for creating and gathering asyncio tasks.
+"""
+
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Uses task_wait_random to create tasks, gather their results,
+    and return the delays in ascending order.
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
