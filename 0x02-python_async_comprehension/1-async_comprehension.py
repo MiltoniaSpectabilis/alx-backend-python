3fc64c4463c0874f18bd9e2667654563a6669fa5
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-10-16 03:08:11 +0100

    add type annotation

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,11 +3,11 @@
 """
 Module for collecting random numbers using an async comprehension.
 """
-
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     Collects 10 random numbers using an async comprehension
     over async_generator.
