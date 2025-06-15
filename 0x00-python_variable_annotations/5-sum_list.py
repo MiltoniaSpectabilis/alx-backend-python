c97c2ec824d77063e9bccab175519094d3f1b164
MiltoniaSpectabilis <125466560+MiltoniaSpectabilis@users.noreply.github.com>
2025-06-15 19:08:15 +0100

    Implement a py script that returns the sum of a list of floats with annotations

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module containes a function that returns the sum
+of a list of floats with annotations
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Returns the sum of a list of floats."""
+    return sum(input_list)
