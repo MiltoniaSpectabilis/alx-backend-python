b7e7bf91e3d0a8018d42026707f70b230a5a8a60
MiltoniaSpectabilis <125466560+MiltoniaSpectabilis@users.noreply.github.com>
2025-06-15 19:09:28 +0100

    Import List from typing

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,7 +4,9 @@ This module containes a function that returns the sum
 of a list of floats with annotations
 """
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """Returns the sum of a list of floats."""
     return sum(input_list)
