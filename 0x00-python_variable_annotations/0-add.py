c26301891909bf0a81959fd3c95c0a4f5f4a6e35
MiltoniaSpectabilis <125466560+MiltoniaSpectabilis@users.noreply.github.com>
2025-06-15 03:44:37 +0100

    Implement a simple py script that returns the sum of two vars with annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module returns the sum of two variables
+with annotations
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Sums two variables"""
+    return a + b
